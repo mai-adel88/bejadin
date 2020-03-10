1d0c8a595e773aed180b4244afde1da91f64fa40
A.Hamouda <ahmedsherbeny996@gmail.com>
2020-03-10 17:32:10 +0200

    add HrOwnrmf tabel and model

---
+++ app/Models/Hr/HrOwnrmf.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Hr;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HrOwnrmf extends Model
+{
+    protected $table = 'HROwnrmf';
+    public $timestamps = true;
+    protected $primaryKey = 'ID_No';
+    protected $guarded = [];
+    
+}
